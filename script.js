fe8ae731ffff1df0b6522e7ff7f3060b15a7b33b
seu-username <dev.web.carlos@gmail.com>
2025-09-18 13:59:09 -0300

    Sorteador de numeros aleatorios

---
+++ script.js
@@ -0,0 +1,21 @@
+const buttonSorteio = document.querySelector('.button-draw')
+const drawnNumber = document.querySelector('.drawn-number')
+
+function Sortear() {
+
+    const min = Math.ceil(document.querySelector('.min').value)
+    const max = Math.floor(document.querySelector('.max').value)
+
+    if (max < min) {
+        alert("O valor MÁXIMO deve ser maior que o valor MÍNIMO.");
+        return; 
+    }
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    drawnNumber.innerHTML = `O ganhador foi o número ${result}`;
+}
+
+
+
+buttonSorteio.addEventListener('click', Sortear)
